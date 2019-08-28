e886b6bbf0aa9c6919930b8c23497d135a59baea
aldeez988 <aldeez911088@gmail.com>
2019-08-28 17:14:58 +0100

    adding primary button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,37 @@
 import React from "react";
+import styled from 'styled-components';
 
-const Button = props => (
-  <button
-    onClick={props.onClick}
-    className={props.className}
-    style={props.style}
-  >
+
+const btnPrimary = {
+  name :"btnPrimary",
+  color: "#ffffff",
+  background: "#2196f3"
+}
+
+const buttonTypes = [
+  btnPrimary
+] 
+const Button = styled.button`
+   font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  font-family: 'Open Sans', sans-serif;
+  line-height: 14px;
+   color: ${props=>buttonTypes.find(btn=>btn.name ===props.type).color};
+  display: flex;
+  align-items: center;
+  text-align: center;
+  letter-spacing: 0.75px;
+  text-transform: capitalize;
+  padding: 8px 30px;
+  border-radius: 5px;
+  border: none;
+  background : ${props=>buttonTypes.find(btn=>btn.name ===props.type).background};
+`;
+
+export default  props => (
+  <Button type={props.type}>
     {props.label && <span>{props.label}</span>}
-  </button>
+  </Button>
 );
-export default Button;
+//export default Button;
--- src/stories/buttonStories.js
+++ src/stories/buttonStories.js
@@ -11,197 +11,198 @@ storiesOf("Button", module)
       onClick={action("click")}
       label="Button"
       className="btn btn-primary"
+      type="btnPrimary"
     />
   ))
-  .add("primary outline", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-primary-outline"
-    />
-  ))
-  .add("primary-light", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-primary-light"
-    />
-  ))
-  .add("primary with gray shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-primary-gray-shadow"
-    />
-  ))
-  .add("primary with shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-primary-shadow"
-    />
-  ))
-  // secondary Button
-  .add("secondary", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-secondary"
-    />
-  ))
-  .add("secondary outline", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-secondary-outline"
-    />
-  ))
-  .add("secondary-light", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-secondary-light"
-    />
-  ))
-  .add("secondary with gray shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-secondary-gray-shadow"
-    />
-  ))
-  .add("secondary with shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-secondary-shadow"
-    />
-  ))
-  // error Button
-  .add("error", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-error"
-    />
-  ))
-  .add("error outline", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-error-outline"
-    />
-  ))
-  .add("error-light", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-error-light"
-    />
-  ))
-  .add("error with gray shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-error-gray-shadow"
-    />
-  ))
-  .add("error with shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-error-shadow"
-    />
-  ))
-  // devider Button
-  .add("divider", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-devider"
-    />
-  ))
-  .add("devider outline", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-devider-outline"
-    />
-  ))
-  .add("devider-light", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-devider-light"
-    />
-  ))
-  .add("devider with gray shadow", () => (
-    <Button
-      label="Button"
-      onClick={action("click")}
-      className="btn btn-devider-gray-shadow"
-    />
-  ))
-  //rounded buttons
-  .add("button rounded corners with 5px", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-corner-5px"
-    />
-  ))
-  .add("button rounded corners with 200px", () => (
-    <Button
-      onClick={action("click")}
-      label="Button"
-      className="btn btn-corner-200px"
-    />
-  ))
-  .add("chip", () => (
-    <Button onClick={action("click")} label="Chip" className="btn btn-chip" />
-  ))
-  //ellipse Button
-  .add("ellipse", () => (
-    <Button
-      onClick={action("click")}
-      label="+"
-      className="btn btn-primary ellipse"
-    />
-  ))
-  .add("ellipse outline", () => (
-    <Button
-      label="+"
-      onClick={action("click")}
-      className="btn btn-primary-outline ellipse"
-    />
-  ))
-  .add("ellipse-light", () => (
-    <Button
-      onClick={action("click")}
-      label="+"
-      className="btn btn-primary-light ellipse"
-    />
-  ))
-  .add("ellipse with gray shadow", () => (
-    <Button
-      label="+"
-      onClick={action("click")}
-      className="btn btn-primary-gray-shadow ellipse"
-    />
-  ))
-  .add("ellipse with shadow", () => (
-    <Button
-      label="+"
-      onClick={action("click")}
-      className="btn btn-primary-shadow ellipse"
-    />
-  ))
-  //secondary with vector
-  .add("secondary with vector", () => (
-    <Button
-      label="+ Button"
-      onClick={action("click")}
-      className="btn btn-secondary"
-    />
-  ))
-  //
+  // .add("primary outline", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-outline"
+  //   />
+  // ))
+  // .add("primary-light", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-primary-light"
+  //   />
+  // ))
+  // .add("primary with gray shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-gray-shadow"
+  //   />
+  // ))
+  // .add("primary with shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-shadow"
+  //   />
+  // ))
+  // // secondary Button
+  // .add("secondary", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-secondary"
+  //   />
+  // ))
+  // .add("secondary outline", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-secondary-outline"
+  //   />
+  // ))
+  // .add("secondary-light", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-secondary-light"
+  //   />
+  // ))
+  // .add("secondary with gray shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-secondary-gray-shadow"
+  //   />
+  // ))
+  // .add("secondary with shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-secondary-shadow"
+  //   />
+  // ))
+  // // error Button
+  // .add("error", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-error"
+  //   />
+  // ))
+  // .add("error outline", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-error-outline"
+  //   />
+  // ))
+  // .add("error-light", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-error-light"
+  //   />
+  // ))
+  // .add("error with gray shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-error-gray-shadow"
+  //   />
+  // ))
+  // .add("error with shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-error-shadow"
+  //   />
+  // ))
+  // // devider Button
+  // .add("divider", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-devider"
+  //   />
+  // ))
+  // .add("devider outline", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-devider-outline"
+  //   />
+  // ))
+  // .add("devider-light", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-devider-light"
+  //   />
+  // ))
+  // .add("devider with gray shadow", () => (
+  //   <Button
+  //     label="Button"
+  //     onClick={action("click")}
+  //     className="btn btn-devider-gray-shadow"
+  //   />
+  // ))
+  // //rounded buttons
+  // .add("button rounded corners with 5px", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-corner-5px"
+  //   />
+  // ))
+  // .add("button rounded corners with 200px", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="Button"
+  //     className="btn btn-corner-200px"
+  //   />
+  // ))
+  // .add("chip", () => (
+  //   <Button onClick={action("click")} label="Chip" className="btn btn-chip" />
+  // ))
+  // //ellipse Button
+  // .add("ellipse", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="+"
+  //     className="btn btn-primary ellipse"
+  //   />
+  // ))
+  // .add("ellipse outline", () => (
+  //   <Button
+  //     label="+"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-outline ellipse"
+  //   />
+  // ))
+  // .add("ellipse-light", () => (
+  //   <Button
+  //     onClick={action("click")}
+  //     label="+"
+  //     className="btn btn-primary-light ellipse"
+  //   />
+  // ))
+  // .add("ellipse with gray shadow", () => (
+  //   <Button
+  //     label="+"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-gray-shadow ellipse"
+  //   />
+  // ))
+  // .add("ellipse with shadow", () => (
+  //   <Button
+  //     label="+"
+  //     onClick={action("click")}
+  //     className="btn btn-primary-shadow ellipse"
+  //   />
+  // ))
+  // //secondary with vector
+  // .add("secondary with vector", () => (
+  //   <Button
+  //     label="+ Button"
+  //     onClick={action("click")}
+  //     className="btn btn-secondary"
+  //   />
+  // ))
+  // //
