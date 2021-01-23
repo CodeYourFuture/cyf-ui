b23652301fe7d54f2d0894f3c4e2930bb3d8fec2
Kyaw <40131560+kiowd@users.noreply.github.com>
2021-01-23 16:21:08 +0000

    wrote some basic tests and gave path TabLabel

--- src/components/Tabs/_tests_/Tab.test.js
+++ src/components/Tabs/_tests_/Tab.test.js
@@ -18,38 +18,38 @@ import { TabLabel } from "../TabLabel";
 // });
 
 describe('<TabLabel/>', () => {
-    test('renders without crushing', () => {
-        let activeTab = false;
-        let label = "";
-        const clickTabItem = jest.fn(() => null);
-        render(<TabLabel            
-            active={!activeTab}
-            label={label}
-            clickTabItem={clickTabItem}
-          />);
-    });
-    test('clickTab', () => {
-        let clicked = false;
-        let activeTab = false;
-        const testLabel = "NewLabel"
-
-        const { getByTestId } = render(<TabLabel active={!activeTab}
-                                         label={testLabel} clickTabItem={() =>clicked = true}/>);
-        const tabList = getByTestId('label');
-        //expect(getByTestId('label')).toHaveTextContent(testLabel);
-        fireEvent.click(tabList);
-        expect(clicked).toBe(true);
-
-    });
-    // test('clickTab with mocked function', () => {
+    // test('renders without crushing', () => {
     //     let activeTab = false;
     //     let label = "";
-    //     const click = jest.fn(() => null);
+    //     const clickTabItem = jest.fn(() => null);
+    //     render(<TabLabel            
+    //         active={!activeTab}
+    //         label={label}
+    //         clickTabItem={clickTabItem}
+    //       />);
+    // });
+    // test('clickTab', () => {
+    //     let clicked = false;
+    //     let activeTab = false;
+    //     const testLabel = "NewLabel"
+
     //     const { getByTestId } = render(<TabLabel active={!activeTab}
-    //         label={label} clickTabItem={click}/>);
-    //     const tabList = getByTestId('tabs');
+    //                                      label={testLabel} clickTabItem={() =>clicked = true}/>);
+    //     const tabList = getByTestId('label');
+    //     //expect(getByTestId('label')).toHaveTextContent(testLabel);
     //     fireEvent.click(tabList);
-    //     expect(click).toBeCalled();
+    //     expect(clicked).toBe(true);
 
     // });
+    test('clickTab with mocked function', () => {
+        let activeTab = false;
+        let label = "Steps";
+        const click = jest.fn(() => null);
+        const { getByText } = render(<TabLabel active={!activeTab}
+            label={label} clickTabItem={click}/>);
+        const tabList = getByText(label);
+        fireEvent.click(tabList);
+        expect(click).toBeCalled();
+
+    });
 })
